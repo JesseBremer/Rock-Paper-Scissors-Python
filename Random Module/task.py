1fba18f8bd978ff05c5fce69d61a82baceedc478
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-26 09:51:33 -0400

    Heads or Tails checker.

--- Random Module/task.py
+++ Random Module/task.py
@@ -1,7 +1,24 @@
 import random
-import my_module
 
-randomInt = random.randint(1,10)
+# import my_module
+# print(my_module.my_fav_num)
+#
+# randomInt = random.randint(1,10)
+#
+# print(randomInt)
+#
+# random_number_0to1 = random.random() * 10
+# print(random_number_0to1)
+#
+# random_float = random.uniform(1, 10)
+# print(random_float)
 
-print(randomInt)
-print(my_module.my_fav_num)
+# Heads or Tails
+
+coin = random.randint(1,2)
+print(coin)
+
+if coin == 1:
+    print("Heads")
+else:
+    print("Tails")
