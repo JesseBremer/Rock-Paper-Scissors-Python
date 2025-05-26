1a9e7dc4917c23982519eb71d54c15e217b2cfc5
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-26 11:04:57 -0400

    Added index variable for comparison.

--- Rock Paper Scissors/task.py
+++ Rock Paper Scissors/task.py
@@ -29,16 +29,31 @@ scissors = '''
 ---.__(___)
 '''
 
-get user choice
-get computer choice
-compare against
-decide winner
-
-
-
 game_choices.extend([rock, paper, scissors])
 
 
+# compare against
+# decide winner
+
+user_selection = int(input("Choose your weapon: 0 - Rock, 1 - Paper, 2 - Scissors!\n"))
+cpu_selection = random.choice(game_choices)
+cpu_selection_index = game_choices.index(cpu_selection)
 
 
-print(random.choice(game_choices))
+# Game Logic
+# Rock
+if user_selection == cpu_selection:
+    print(game_choices[user_selection])
+    print("Computer Chose:")
+    print(cpu_selection)
+    print("It's a tie.")
+elif user_selection == 0 and cpu_selection == game_choices[1]:
+    print(game_choices[user_selection])
+    print("Computer Chose:")
+    print(cpu_selection)
+    print("You Lose.")
+elif user_selection == 0 and cpu_selection == game_choices[2]:
+    print(game_choices[user_selection])
+    print("Computer Chose:")
+    print(cpu_selection)
+    print("You Win!")
