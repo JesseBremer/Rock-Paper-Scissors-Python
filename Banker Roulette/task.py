42fa040366458f0619dd8be9877383a583a3a395
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-26 10:13:58 -0400

    Using random module with lists.

--- Banker Roulette/task.py
+++ Banker Roulette/task.py
@@ -1,1 +1,8 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+amount_of_friends = len(friends)
+
+random_selector = random.randint(0, amount_of_friends -1)
+
+print(friends[random_selector])
